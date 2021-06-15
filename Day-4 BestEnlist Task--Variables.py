6117084f19c95d02c9385225ff915b0310474b28
saiprasanna3 <75064036+saiprasanna3@users.noreply.github.com>
2021-06-15 15:44:24 +0530

    Task on variables

---
+++ Day-4 BestEnlist Task--Variables.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+1.Create three variables(a,b,c) to same value of any integer &do the following 
+  a) Divide a by 10
+  b) Multiply b by 50
+  c) Add c value by 60
+# In[1]:
+
+
+a=b=c=565
+print('Divide a by 10')
+print('a/10=',a/10)
+print()
+print('Multiply b by 50')
+print('b*50=',b*50)
+print()
+print('Add c by 60')
+print('c+60=',c+60)
+
+2.Create a String variable of 5 characters and replace 3rd character with G
+# In[2]:
+
+
+string='MACHINE LEARNING'
+x=string.replace(string[3],'G')
+
+
+# In[3]:
+
+
+print(x)
+
+3.Create two values (a,b) of int,float data type& convert them viseversa.
+# In[4]:
+
+
+a,b=66,98.6
+a1=('float conversion of 66 is',float(a))
+b1=('int conversion of 98.6 is',int(b))
+
+
+# In[5]:
+
+
+print(a1,b1)
+
